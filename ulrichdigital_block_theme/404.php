5741549ace63ca1de883018fd74601afd9fab337
ulrich-digital <post@ulrich.digital>
2026-03-27 17:05:23 +0100

    Delete 404.php

--- ulrichdigital_block_theme/404.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Falls 404-Block-Template existiert, wird dieses i. d. R. bevorzugt.
-// Diese Datei greift nur, wenn kein 404.html vorhanden ist.
-wp_safe_redirect(home_url('/'), 301);
-exit;
